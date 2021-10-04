c039d56c066b4a0055e7460cae552062d6cbf4bb
Adriano Pimenta <apimenta.usp@gmail.com>
2021-10-04 14:29:53 -0300

    Criando a pasta 'Personalizacao' e algumas classes

---
+++ InterfaceUsuario/Personalizacao/ControleDeImagens.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace InterfaceUsuario.Personalizacao {
+    public class ControleDeImagens {
+        public static void InserirImagens(Button botaoPrincipal, Bitmap imagemPrincipal,
+                            Button botaoSecundario, Bitmap imagemSecundaria) {
+            botaoPrincipal.BackgroundImage = imagemPrincipal;
+            botaoPrincipal.BackgroundImageLayout = ImageLayout.Zoom;
+
+            botaoSecundario.BackgroundImage = imagemSecundaria;
+            botaoSecundario.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+
+        public static void TrocarImagem(Button botao, Bitmap imagem) {
+            botao.BackgroundImage = imagem;
+            botao.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+    }
+}
---
+++ InterfaceUsuario/Personalizacao/Visor.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace InterfaceUsuario.Personalizacao {
+    public class Visor {
+        public static string MostrarNoVisor(double resultado) {
+            bool virgula = true;
+            string visor = resultado.ToString(CultureInfo.InvariantCulture);
+            /* Obs.: Este método vai substituir (replace) 
+               o ponto por vírgula, caso o separador decimal 
+               "vírgula" esteja ativado. 
+               Ou seja, o booleano Virgula for true. */
+            if (virgula) {
+                visor.Replace('.', ',');
+            }
+            return visor;
+        }
+    }
+}
